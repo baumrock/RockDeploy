5a86188abc33120a14d4f8b82600441bfb5b7b88
BernhardBaumrock <office@baumrock.com>
2022-04-15 16:57:47 +0200

    load processwire

--- Deployment.php
+++ Deployment.php
@@ -1,12 +1,34 @@
 <?php namespace RockDeploy;
-class Deployment {
+
+use ProcessWire\WireData;
+
+// we make sure that the current working directory is the PW root
+chdir(dirname(dirname(dirname(__DIR__))));
+require_once "wire/core/ProcessWire.php";
+class Deployment extends WireData {
+
+  public $paths;
+
+  public function __construct() {
+    $this->paths = new WireData();
+    $this->paths->pwroot = getcwd();
+    $this->paths->deploydir = dirname($this->paths->pwroot);
+  }
 
   public function hello() {
     $this->write("Hi there, I'm RockDeploy...");
   }
 
+  /**
+   * Print paths
+   */
+  public function paths() {
+    $this->write($this->paths->getArray());
+  }
+
   public function write($msg) {
-    echo "$msg\n";
+    if(is_array($msg)) echo print_r($msg, true);
+    else echo "$msg\n";
   }
 
 }
