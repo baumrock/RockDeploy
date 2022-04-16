d4a395e43eb0ba06c69a55814dccb20561fee92d
BernhardBaumrock <office@baumrock.com>
2022-04-16 15:15:43 +0200

    logging

--- Deployment.php
+++ Deployment.php
@@ -127,12 +127,12 @@ class Deployment extends WireData {
       include "$current/site/config.php";
       $dir = "$current/site/assets/backups/database";
       $sql = "$dir/rockdeploy.sql";
-      $this->echo("  ".realpath($sql));
-      $this->echo("  ".str_replace("/site/", "-/site/", realpath($sql)));
       $this->exec("
         mkdir -p $dir
         mysqldump -u'{$config->dbUser}' -p'{$config->dbPass}' {$config->dbName} > $sql
       ");
+      $this->echo(realpath($sql), 2);
+      $this->echo(str_replace("/site/", "-/site/", realpath($sql)), 2);
       $this->echo("Done");
     } catch (\Throwable $th) {
       $this->echo($th->getMessage());
